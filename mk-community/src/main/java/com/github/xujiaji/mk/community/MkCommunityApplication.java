029438357a8307859007616123def6e6370786b9
xujiaji <jiajixu@qq.com>
2020-11-09 17:19:33 +0800

    feat(community): 恢复文件

---
+++ mk-community/src/main/java/com/github/xujiaji/mk/community/MkCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.github.xujiaji.mk.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkCommunityApplication.class, args);
+	}
+
+}
